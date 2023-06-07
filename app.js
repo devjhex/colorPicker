b073ef02b1872e421225775b2fd2fabe7181b66d
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2023-06-07 14:19:07 +0300

    Initial commit of the color picker

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    let random = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[random];
+    color.textContent = colors[random];
+})
+
+function getRandomNumber(){
+   return Math.floor(Math.random()*colors.length);
+}
